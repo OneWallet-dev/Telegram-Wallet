f3ee6fffdd761bdbd06da30d6854ba76ecb843c8
Borodur <107106869+Brdur@users.noreply.github.com>
2022-11-19 21:30:37 +0300

    Update bot.py

--- CryptoBot/Bot/bot.py
+++ CryptoBot/Bot/bot.py
@@ -5,7 +5,7 @@ from aiogram.client.session import aiohttp
 from aiogram.fsm.storage.redis import RedisStorage
 
 from AllLogs.bot_logger import BotLogger
-from Bot.handlers import start_hand
+from Bot.handlers import start_hand, wallet_hand
 from Databases.DB_Postgres import create_session
 from Databases.DB_Redis import RedRedis
 from bata import Data
@@ -18,15 +18,17 @@ dp = Dispatcher(storage=storage)
 async def bot_start():
     bot = Data.main_bot
     bot_info = await bot.get_me()
-    bot["db"] = await create_session()
     print(f"Bot was reborn as\n"
           f"|- {bot_info.full_name} -|- @{bot_info.username} -|\n"
           f"What a time to be alive!\n")
 
     dp.include_router(start_hand.router)
+    dp.include_router(wallet_hand.router)
 
     session = aiohttp.ClientSession()
     await session.close()
+
+    await RedRedis.connect_to_storage()
 
     await bot.delete_webhook(drop_pending_updates=True)
     await dp.start_polling(bot)
