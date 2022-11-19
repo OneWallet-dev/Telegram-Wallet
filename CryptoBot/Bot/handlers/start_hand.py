8871ac9eaee4d6ac92b960f4cb0cf7be1eb57a44
What do you do? <109747879+Whitev2@users.noreply.github.com>
2022-11-20 03:20:00 +0500

    database in middleware

--- CryptoBot/Bot/handlers/start_hand.py
+++ CryptoBot/Bot/handlers/start_hand.py
@@ -4,10 +4,11 @@ from aiogram import Router, F
 from aiogram.filters import Command
 from aiogram.fsm.context import FSMContext
 from aiogram.types import Message
+from sqlalchemy import select
 from sqlalchemy.exc import IntegrityError
 from sqlalchemy.ext.asyncio import AsyncSession
 
-from Bot.keyboards.main_keys import main_kb
+from Bot.keyboards.main_keys import start_kb
 from Bot.states.main_states import MainState
 from Databases.models.user import Users
 from bata import Data
@@ -26,7 +27,8 @@ async def commands_start(message: Message, state: FSMContext, session: AsyncSess
     user.username = message.from_user.username
     user.first_name = message.from_user.first_name
     session.add(user)
-    await session.commit()
+    with suppress(IntegrityError):
+        await session.commit()
 
     await state.set_state(MainState.welcome_state)
-    await message.answer('BOT IS ALIVE', reply_markup=main_kb())
+    await message.answer('BOT IS ALIVE', reply_markup=start_kb())
