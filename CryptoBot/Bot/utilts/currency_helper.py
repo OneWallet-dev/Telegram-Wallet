522dfcb142c07b6f2105778d05dd0e80d4080313
What do you do? <109747879+Whitev2@users.noreply.github.com>
2022-11-29 21:56:02 +0500

    network_dict

--- CryptoBot/Bot/utilts/currency_helper.py
+++ CryptoBot/Bot/utilts/currency_helper.py
@@ -1,3 +1,8 @@
 base_tokens = {
     'USDT': {'network': ['TRC-20'], 'contract_address': "TR7NHqjeKQxGTCi8q8ZY4pL8otSzgjLj6t"}
 }
+
+network_dict = {
+    "tron": ["TRC-20", "TRC-10"],
+    'ethereum': ["ERC-20"], 'bitcoin': ["bitcoin"]
+}
