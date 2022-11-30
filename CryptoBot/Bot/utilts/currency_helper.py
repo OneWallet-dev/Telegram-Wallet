1b794bfdfc5f3379a0834f2145a03a5e64ab5420
What do you do? <109747879+Whitev2@users.noreply.github.com>
2022-12-01 04:08:01 +0500

    transfer in testnet

--- CryptoBot/Bot/utilts/currency_helper.py
+++ CryptoBot/Bot/utilts/currency_helper.py
@@ -1,6 +1,7 @@
 base_tokens = {
     'USDT': {'network': ['TRC-20'], 'blockchain': 'tron',
-             'contract_address': "TR7NHqjeKQxGTCi8q8ZY4pL8otSzgjLj6t"}
+             'contract_address': "TR7NHqjeKQxGTCi8q8ZY4pL8otSzgjLj6t",
+             'testnet_contract_address': "TXLAQ63Xg1NAzckPwKHvzw7CSEmLMEqcdj"}
 }
 
 
