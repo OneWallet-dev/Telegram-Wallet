e749bc43b1e8411f3bf3992eb4df74d1123ab342
AntipropPrject <antipropoganda2402@gmail.com>
2022-12-01 18:20:27 +0300

    transactions new columns

--- CryptoBot/Dao/models/Transaction.py
+++ CryptoBot/Dao/models/Transaction.py
@@ -1,15 +1,22 @@
 import datetime
 
-from sqlalchemy import Column, BigInteger, Float, String, ForeignKey, DateTime
+from sqlalchemy import Column, BigInteger, Float, String, ForeignKey, DateTime, Boolean
+from sqlalchemy.orm import relationship
 
 from Dao.DB_Postgres.session import Base
+from Dao.models.Token import Token
 
 
 class Transaction(Base):
     __tablename__ = "transactions"
 
     id = Column(BigInteger, primary_key=True, unique=True, autoincrement=True)
+    tnx_id: str = Column(String)
+    token: Token=Column(String, ForeignKey('tokens.token_name', onupdate="CASCADE", ondelete="CASCADE"))
     amount = Column(Float)
     from_wallet = Column(String, ForeignKey('addresses.address', onupdate="CASCADE", ondelete="CASCADE"))
     to_wallet = Column(String)
     datetime = Column(DateTime, default=datetime.datetime.now())
+    status:bool = Column(Boolean)
+    service_fee: float = Column(Float)
+    network_fee: float = Column(Float)
