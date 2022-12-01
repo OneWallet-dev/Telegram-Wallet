9d266f3c1ed336aeb8865d9b7a5251e3f3305a4e
Borodur <107106869+Brdur@users.noreply.github.com>
2022-12-01 15:11:54 +0300

    Update owner_service.py

--- CryptoBot/Services/owner_service.py
+++ CryptoBot/Services/owner_service.py
@@ -44,7 +44,7 @@ class OwnerService:
         async with session_connect() as session:
             token_ref = base_tokens.get(token)
 
-            address = await OwnerService.get_сhain_address(session, user, token_ref['blockchain'])
+            address = await OwnerService.get_сhain_address(user.id, token_ref['blockchain'])
             token_obj = await TokenService.get_token(base_tokens[token]['contract_address'])
             if not token_obj:
                 token_obj = Token(token_name=token,
@@ -59,12 +59,12 @@ class OwnerService:
                 raise DuplicateToken
 
     @staticmethod
-    async def get_сhain_address(session: AsyncSession, user: User, blockchain: str, path_index: int = 0):
+    async def get_сhain_address(user_id: int, blockchain: str, path_index: int = 0):
         session_connect = await create_session()
         async with session_connect() as session:
             address: Address = (await session.execute(
                 select(Address).where(
                     Address.path_index == path_index, Address.wallet_id == select(Wallet.id).where(
-                        Wallet.owner_id == str(user.id), Wallet.blockchain == blockchain).scalar_subquery()))
+                        Wallet.owner_id == str(user_id), Wallet.blockchain == blockchain).scalar_subquery()))
                                 ).first()[0]
             return address
