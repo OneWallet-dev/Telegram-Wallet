bc7610c5377710f8f0951b0a673572326d2797f7
What do you do? <109747879+Whitev2@users.noreply.github.com>
2022-12-14 18:37:37 +0500

    eth transfer

--- CryptoBot/Services/CryptoMakers/ETH/ETH_wallet.py
+++ CryptoBot/Services/CryptoMakers/ETH/ETH_wallet.py
@@ -1,26 +1,43 @@
 import asyncio
 import math
-
 from web3 import Web3, AsyncHTTPProvider
 from web3.eth import AsyncEth
-from web3.geth import Geth, AsyncGethTxPool, AsyncGethPersonal, AsyncGethAdmin
 from web3.net import AsyncNet
+from web3.geth import Geth, AsyncGethTxPool, AsyncGethPersonal, AsyncGethAdmin
+from ethtoken.abi import EIP20_ABI
+
+mnemonic = "rather movie picnic soft wreck sign dial mango laugh matrix opera below"
+wallet_1 = {"address": "0x6f9c2F6f96481848BC39419B53719EAD68FE9F4b",
+            "private_key": "0x04e2bfb6457223596d62a1857e65cf745cbfb80eadaaf9d7f967f8ba77f90863"}
+wallet_2 = {"address": "0xb1E2e0166AC14769d0688FE4f29eb28f74eD90ea",
+            "private_key": "0x2a312af84096ef7184e8e08a8ac0018186911e6038e6fa796b7d83feb200c317"}
 
 
-class Eth_wallet:
-    def __init__(self, api: str, gas_limit: int, cryptocurrency: str = "mainnet"):
+class ETH_wallet:
+
+    def __init__(self, network: str = "goerli"):
+        self.api_key = "854dcd46b0bc46579d3dba0fbfac496c"
+        self.w3 = None
+        self.signature = None
+        self.txn_resp = dict()
+        self.network = network
+        self.__add_fee = 500_000
         self.__BASE = "https://{}.infura.io/v3/"
-        if cryptocurrency == "goerli":
-            self.network = self.__BASE.format(cryptocurrency)
-        elif cryptocurrency == "mainnet":
-            self.network = self.__BASE.format(cryptocurrency)
+        self.__get_api_base()
+        self.__get_w3()
+
+    def __get_api_base(self):
+        if self.network == "goerli":
+            self.__BASE = self.__BASE.format(self.network)
+        elif self.network == "mainnet":
+            self.__BASE = self.__BASE.format(self.network)
         else:
             raise ValueError("BadNetwork")
-        self.API = api
-        self.gas_limit = gas_limit
+        return self
 
+    def __get_w3(self):
         self.w3 = Web3(
-            AsyncHTTPProvider(self.network + self.API),
+            AsyncHTTPProvider(self.__BASE + self.api_key),
             modules={'eth': (AsyncEth,),
                      'net': (AsyncNet,),
                      'geth': (Geth,
@@ -30,57 +47,106 @@ class Eth_wallet:
                      },
             middlewares=[]  # See supported middleware section below for middleware options
         )
+        return self
+
+    def __is_valid_token(self, token_name):
+        token_list = [
+            'wei', 'kwei', 'babbage', 'femtoether', 'mwei', 'lovelace',
+            'picoether', 'gwei', 'shannon', 'nanoether', 'nano', 'szabo',
+            'microether', 'micro', 'finney', 'milliether', 'milli', 'ether',
+            'kether', 'grand', 'mether', 'gether', 'tether'
+        ]
+        if token_name in token_list:
+            return True
+        else:
+            return False
+
+    async def get_gas_price(self):
+        return await self.w3.eth.gas_price
+
+    async def get_balance(self, address, contract):
+        balance = await self.w3.eth.get_balance(address)
+        return self.w3.fromWei(balance, "ether")
 
     async def build_txn(self, from_address: str,
                         to_address: str,
                         amount: float,
-                        nonce
+                        nonce,
+                        token: str = "ether",
+                        contract: str = None
                         ) -> dict[str, int | str]:
 
-        gas_price = await self.w3.eth.gas_price
+        gas_price = await self.get_gas_price()
 
-        txn = {
-            'chainId': await self.w3.eth.chain_id,
-            'from': from_address,
-            'to': to_address,
-            'value': int(Web3.toWei(amount, 'ether')),
-            'nonce': nonce,
-            'gasPrice': gas_price,
-            'gas': self.gas_limit,
-        }
-        return txn
+        if contract is None:
+            txn = {
+                'chainId': await self.w3.eth.chain_id,
+                'from': from_address,
+                'to': to_address,
+                'value': int(Web3.toWei(amount, token)),
+                'nonce': nonce,
+                'gasPrice': gas_price,
+                'gas': gas_price + self.__add_fee,
+            }
+            return txn
+        else:
+            unicorns = self.w3.eth.contract(address=contract, abi=EIP20_ABI)
+            nonce = await self.w3.eth.get_transaction_count(from_address, 'pending')
 
-    async def create_transaction(self, from_address: str,
-                                 to_address: str,
-                                 amount: float,
-                                 private_key: str
-                                 ):
+            gas_price = await self.get_gas_price()
+            unicorn_txn = unicorns.functions.transfer(
+                to_address=to_address,
+                amount=amount,
+            ).buildTransaction({
+                'chainId': 1,
+                'gas': gas_price + self.__add_fee,
+                'gasPrice': gas_price,
+                'nonce': nonce,
+            })
+            return unicorn_txn
+
+    async def transfer(self, from_address: str,
+                       to_address: str,
+                       amount: float,
+                       private_key: str,
+                       contract: str = None
+                       ):
         nonce = await self.w3.eth.get_transaction_count(from_address, 'pending')
-        try:
+        if contract is None:
+            transaction = await self.build_txn(from_address, to_address, amount, nonce)
             try:
-                transaction = await self.build_txn(from_address, to_address, amount, nonce)
-                signed_txn = self.w3.eth.account.sign_transaction(transaction, private_key)
-                txn_hash = await self.w3.eth.send_raw_transaction(signed_txn.rawTransaction)
-            except ValueError:
-                nonce += math.ceil(round(nonce / 8, 3))
-                print(nonce)
-                transaction = await self.build_txn(from_address, to_address, amount, nonce)
-                signed_txn = self.w3.eth.account.sign_transaction(transaction, private_key)
-                txn_hash = await self.w3.eth.send_raw_transaction(signed_txn.rawTransaction)
+                try:
+                    signed_txn = self.w3.eth.account.sign_transaction(transaction, private_key)
+                    txn_hash = await self.w3.eth.send_raw_transaction(signed_txn.rawTransaction)
+                except ValueError:
+                    nonce += math.ceil(round(nonce / 8, 3))
 
+                    signed_txn = self.w3.eth.account.sign_transaction(transaction, private_key)
+                    txn_hash = await self.w3.eth.send_raw_transaction(signed_txn.rawTransaction)
+
+                return txn_hash.hex()
+            except ValueError:
+                return {'Error': 801, 'message': 'Little gas'}
+
+        else:
+            transaction = await self.build_txn(from_address, to_address, amount, nonce, contract)
+
+            signed_txn = self.w3.eth.account.sign_transaction(self.build_txn, private_key=private_key)
+            txn_hash = await self.w3.eth.send_raw_transaction(signed_txn.rawTransaction)
             return txn_hash.hex()
-        except ValueError:
-            return {'Error': 801, 'message': 'Little gas'}
+
+
+
 
 
 async def main():
-    eth = Eth_wallet(api="854dcd46b0bc46579d3dba0fbfac496c", gas_limit=50_000, cryptocurrency="goerli")
-    tr_id = await eth.create_transaction(from_address="0xdd7622DA4742fb8152e6f7DD316fc0BC872FBCdb",
-                                         to_address="0xfD8A1C137D6bB676a182A2db9e76f2815A45f083",
-                                         amount=0.0000002,
-                                         private_key="bba719bdf84c830baa39aa908c7d1dedb80c86b86b7201ffd12f3193b7c4a170")
+    eth = ETH_wallet(network="goerli")
+    #tr_id = await eth.transfer(from_address=wallet_1.get("address"),
+    #                           to_address=wallet_2.get("address"),
+    #                           amount=0.0000001,
+    #                           private_key=wallet_1.get("private_key"))
 
-    print(f"Transfer: ", tr_id)
+    #print(f"Transfer: ", tr_id)
 
 
 if __name__ == '__main__':
