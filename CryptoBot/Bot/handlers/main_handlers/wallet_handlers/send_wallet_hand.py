484f418d034860d0646cf786f1d3c9c9eb1261ef
Borodur <107106869+Brdur@users.noreply.github.com>
2022-12-27 19:02:35 +0300

    Update send_wallet_hand.py

--- CryptoBot/Bot/handlers/main_handlers/wallet_handlers/send_wallet_hand.py
+++ CryptoBot/Bot/handlers/main_handlers/wallet_handlers/send_wallet_hand.py
@@ -150,7 +150,7 @@ async def choosen_address(callback: CallbackQuery, bot: Bot, state: FSMContext, 
     placeholder_text = f"Выбранный адрес:\n\n<code>{address.address}</code>\n\nБаланс: {balance[token_name]}\n\n" \
                 f"Выберите сумму которую хотите отправить:"
     if content.text:
-        content.text = content.text.format(address=address.address, token=token_name)
+        content.text = content.text.format(address=address.address)
     await MManager.content_surf(event=callback, state=state, bot=bot, content_unit=content,
                                 placeholder_text=placeholder_text)
     await state.set_state(Trs_transfer.amount)
@@ -173,6 +173,7 @@ async def choose_amount(message: Message, bot: Bot, state: FSMContext):
 
     if balance - frozen_fee < amount + fee:
         missing = float(amount + fee) - float(balance)
+        content: ContentUnit = await ContentUnit(tag="trans_not_enough").get()
         text = f"Для перевода вам не хватает: {missing} {token_name}\n\n" \
                f"Пожалуйста укажите другую сумму или пополните баланс\n\n" \
                f"Ваш баланс: {balance - frozen_fee} {token_name}\n" \
@@ -183,11 +184,11 @@ async def choose_amount(message: Message, bot: Bot, state: FSMContext):
         text = f"Введите адрес на который хотите отправить {token_name} в сети {algo} или UID пользователя"
         keyboard = None
 
-    await state.update_data(amount=amount)
+        await state.update_data(amount=amount)
 
-    content: ContentUnit = await ContentUnit(tag="trans_choose_where").get()
-    if content.text:
-        content.text.format(token=token_name, network=algo)
+        content: ContentUnit = await ContentUnit(tag="trans_choose_where").get()
+        if content.text:
+            content.text.format(token=token_name, network=algo)
 
     await MManager.content_surf(event=message, state=state, bot=bot, content_unit=content, placeholder_text=text,
                                 keyboard=keyboard)
